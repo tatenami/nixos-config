dccbe352d7ee622d51edc25a2daa909f2d672dae
tatenami <ttnm139th@gmail.com>
2026-04-04 15:33:55 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable }: {
+    # VM環境
+    nixosConfugurations = {
+      vm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+
+        ];
+      }
+    };
+  };
+}
